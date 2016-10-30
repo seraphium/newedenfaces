ec2adefce748f213afd26e5c710d096c86bae0bd
Jackie Zhang <viperking@163.com>
2016-10-30 15:24:56 +0800

    now basic compiled

---
+++ app/components/App.js
@@ -0,0 +1,17 @@
+/**
+ * Created by jackiezhang on 2016/10/30.
+ */
+import React from 'react';
+import {RouteHandler} from 'react-router';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+            <RouteHandler />
+            </div>
+    );
+    }
+}
+
+export default App;
